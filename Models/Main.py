5ba64f74a09a0f11c0d2c5ea6b8332ca4bba4f4a
bonganimasemola <bonganimasemola@icloud.com>
2023-12-09 13:56:02 +0300

    seeded data

--- Models/Main.py
+++ Models/Main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (create_engine, desc,
-    CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
-    Index, Column, DateTime, Integer, String)
+from sqlalchemy import (create_engine, CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
+    Index, Column, Integer, String)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -15,12 +14,11 @@ class Restaurant(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
     
     def __repr__(self):
-        pass
-        return f'()
+        return f'Restaurant: {self.name}'
         
 
 class Customers(Base):
@@ -29,12 +27,11 @@ class Customers(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
         
     def __repr__(self):
-        pass
-        return f'()
+        return f'Customer: {self.name}'
     
     
     
@@ -48,11 +45,45 @@ if __name__ == '__main__':
     session = Session()
     
     
-    restaurant = Restaurant()
-    session.add()
+    # restaurant = Restaurant('Shicken Wangs Place')
+    # session.add(restaurant)
+    # session.commit()
+    
+    Shicken_Wangs_Place = Restaurant(name='Shicken Wangs Place')
+    session.add(Shicken_Wangs_Place)
+    session.commit()
+    Rib_Shack = Restaurant(name='Rib Shack')
+    session.add(Rib_Shack)
+    session.commit()
+    Extra_Ordinary_Ugali = Restaurant(name='ExtraOrdinary Ugali')
+    session.add(Extra_Ordinary_Ugali)
+    session.commit()
+    Poly_Nyama_Choma = Restaurant(name='Poly Nyama Choma')
+    session.add(Poly_Nyama_Choma)
+    session.commit()
+    
+    restaurant = session.query(Restaurant).all()
+    print(restaurant)
+    
+    John_Mbaru = Customers(name='John Mbaru')
+    session.add(John_Mbaru)
+    session.commit()
+    Wangachi = Customers(name='Wangachi')
+    session.add(Wangachi)
+    session.commit()
+    Mwangi_Ace = Customers(name='Mwangi Ace')
+    session.add(Mwangi_Ace)
+    session.commit()
+    Sankofa_King = Customers(name='Sankofa King')
+    session.add(Sankofa_King)
+    session.commit()
+    Sean_Carter = Customers(name='Sean Carter')
+    session.add(Sean_Carter)
+    session.commit()
+    Kendrick_Lamar = Customers(name='Kendrick Lamar')
+    session.add(Kendrick_Lamar)
     session.commit()
     
     
-    customers = Customers()
-    session.add()
-    session.commit()
+    
+   
