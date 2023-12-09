e90baf4083a546038e7e1a6b99e6025f5cb4c8ea
bonganimasemola <bonganimasemola@icloud.com>
2023-12-09 11:50:40 +0300

    deleted extra modules, transitioning to main.py to define schema and then CRUD

--- Models/Main.py
+++ Models/Main.py
@@ -1,4 +1,58 @@
-from restaurant import Restaurant
-from customer import Customer
-from review import Review
+#!/usr/bin/env python3
 
+from sqlalchemy import (create_engine, desc,
+    CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
+    Index, Column, DateTime, Integer, String)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class Restaurant(Base):
+    __tablename__ = 'Restaurant'
+    
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    
+    def __init__(self):
+        pass
+    
+    def __repr__(self):
+        pass
+        return f'()
+        
+
+class Customers(Base):
+    __tablename__ = 'Customers'
+    
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    
+    def __init__(self):
+        pass
+        
+    def __repr__(self):
+        pass
+        return f'()
+    
+    
+    
+    
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+    
+    Session = sessionmaker(bind=engine)
+    
+    session = Session()
+    
+    
+    restaurant = Restaurant()
+    session.add()
+    session.commit()
+    
+    
+    customers = Customers()
+    session.add()
+    session.commit()
